4220cc1e4cb42cbaba5e3fed72eceb90ce39853d
javidlrc <javierjrc55@hotmail.com>
2025-02-07 01:24:22 -0700

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <h1>Javier De Los Reyes {{date('Y-m-d')}}</h1>
+        <h1>Javier De Los Reyes Ayooooo this is my update {{date('Y-m-d')}}</h1>
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
